637f62d0f4a518a243bf4c1c6f5adaf38e09dd5e
steve <21293403+hypersolutions@users.noreply.github.com>
2025-03-31 10:40:32 +0100

    Try setting the exit code

--- test/RockHopper.Test/Program.cs
+++ test/RockHopper.Test/Program.cs
@@ -5,4 +5,4 @@ var builder = RockHopperTestApplicationBuilder
     .AddTestingFramework<Program>();
 
 using var testApplication = await builder.BuildAsync();
-_ = await testApplication.RunAsync();
+Environment.ExitCode = await testApplication.RunAsync();
