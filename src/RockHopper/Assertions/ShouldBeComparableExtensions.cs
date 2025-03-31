6f1975418f949636393032ac82540cc6151a4ed8
steve <21293403+hypersolutions@users.noreply.github.com>
2025-03-31 16:01:34 +0100

    Added more int tests

---
+++ src/RockHopper/Assertions/ShouldBeComparableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using RockHopper.Assertions.Exceptions;
+
+namespace RockHopper.Assertions;
+
+// TODO: Complete others and fix assertion error thrown
+
+/// <summary>
+/// Compares the expected value with the actual value.
+/// </summary>
+[DebuggerStepThrough]
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class ShouldBeComparableExtensions
+{
+    /// <summary>
+    /// Checks that the actual value is greater than the expected value.
+    /// </summary>
+    /// <param name="actual">Actual value</param>
+    /// <param name="expected">Expected value</param>
+    /// <param name="customErrorMessage">Custom error message</param>
+    /// <typeparam name="T">Type to assert</typeparam>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void ShouldBeGreaterThan<T>(this T? actual, T? expected, string? customErrorMessage = null) 
+        where T : IComparable<T>?
+    {
+        if (actual?.CompareTo(expected) <= 0)
+            throw new ExpectedAndActualException<T>(expected, actual, customErrorMessage);
+    }
+}
---
+++ test/RockHopper.IntTest/DataFromConfigurationTests.cs
@@ -0,0 +1,43 @@
+﻿using RockHopper.Assertions;
+using RockHopper.IntTest.Support;
+using RockHopper.Mocking;
+
+namespace RockHopper.IntTest;
+
+public class DataFromConfigurationTests
+{
+    private readonly CalculatorAltService _calculatorService = TestContext.Subject<CalculatorAltService>();
+    private readonly Mock<ICalculator> _calculator = TestContext.Mock<ICalculator>();
+    private readonly int _firstNumber = TestContext.Configuration.Get<int>("TestData:X");
+    private readonly int _secondNumber = TestContext.Configuration.Get<int>("TestData:Y");
+    private readonly int _sumResult = TestContext.Configuration.Get<int>("TestData:Sum");
+    private readonly int _differenceResult = TestContext.Configuration.Get<int>("TestData:Difference");
+
+    public DataFromConfigurationTests()
+    {
+        _firstNumber.ShouldBeGreaterThan(0, "Check we have loaded the first number from config.");
+        _secondNumber.ShouldBeGreaterThan(0, "Check we have loaded the second number from config.");
+        _sumResult.ShouldBeGreaterThan(0, "Check we have loaded the sum from config.");
+        _differenceResult.ShouldBeGreaterThan(0, "Check we have loaded the difference from config.");
+    }
+    
+    [Test]
+    public void TwoNumbers_AddNumbers_ReturnsSum()
+    {
+        _calculator.Setup(c => c.Add(_firstNumber, _secondNumber)).Returns(_sumResult);
+
+        var result = _calculatorService.AddNumbers(_firstNumber, _secondNumber);
+        
+        result.ShouldBe(_sumResult);
+    }
+    
+    [Test]
+    public void TwoNumbers_SubtractNumbers_ReturnsDifference()
+    {
+        _calculator.Setup(c => c.Subtract(_firstNumber, _secondNumber)).Returns(_differenceResult);
+
+        var result = _calculatorService.SubtractNumbers(_firstNumber, _secondNumber);
+        
+        result.ShouldBe(_differenceResult);
+    }
+}
