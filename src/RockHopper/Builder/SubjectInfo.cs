4dd97fbf73f1aad9d0d1fded4b4028a976747eea
steves <21293403+hypersolutions@users.noreply.github.com>
2025-04-23 11:40:46 +0100

    Added lock on subject create

--- src/RockHopper/Builder/SubjectInfo.cs
+++ src/RockHopper/Builder/SubjectInfo.cs
@@ -6,25 +6,43 @@ namespace RockHopper.Builder;
 
 internal sealed class SubjectInfo
 {
+    private readonly ReaderWriterLockSlim _subjectCreateLock = new();
     private List<Mock>? _mocks;
     private object? _subject;
         
     internal TSubject GetSubject<TSubject>(SubjectBuilderFlags builderFlags, IConstructorSelector constructorSelector)
     {
-        // TODO: Thread safe?
-        if (_subject is not null)
+        try
         {
-            return (TSubject)_subject;
-        }
+            _subjectCreateLock.EnterUpgradeableReadLock();
+            
+            if (_subject is not null)
+            {
+                return (TSubject)_subject;
+            }
 
-        var subjectInfoCache = SubjectInfoCache.Get<TSubject>(constructorSelector, builderFlags);
-        var parameterMocks = subjectInfoCache.BuildParameterMocks();
-        var propertyMocks = subjectInfoCache.BuildPropertyMocks();
+            try
+            {
+                _subjectCreateLock.EnterWriteLock();
+                
+                var subjectInfoCache = SubjectInfoCache.Get<TSubject>(constructorSelector, builderFlags);
+                var parameterMocks = subjectInfoCache.BuildParameterMocks();
+                var propertyMocks = subjectInfoCache.BuildPropertyMocks();
         
-        _subject = subjectInfoCache.CreateSubject<TSubject>(parameterMocks, propertyMocks)!;
-        _mocks = subjectInfoCache.BuildMocks(parameterMocks, propertyMocks);
+                _subject = subjectInfoCache.CreateSubject<TSubject>(parameterMocks, propertyMocks)!;
+                _mocks = subjectInfoCache.BuildMocks(parameterMocks, propertyMocks);
         
-        return (TSubject)_subject;
+                return (TSubject)_subject;
+            }
+            finally
+            {
+                _subjectCreateLock.ExitWriteLock();
+            }
+        }
+        finally
+        {
+            _subjectCreateLock.ExitUpgradeableReadLock();
+        }
     }
     
     internal Mock<TMock> GetMock<TMock>() where TMock : class
