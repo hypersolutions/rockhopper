8454f9e29211fc3810f67e2d064a9411147d0fdc
steves <21293403+hypersolutions@users.noreply.github.com>
2025-05-29 10:32:37 +0100

    Fix forward slash

---
+++ temp.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+# ver="/refs/tags/v/v0.1.0"
+# echo $ver
+# replacement="/refs/tags/v/"
+# replace_with=""
+# x=${ver#replacement}
+# echo $x
+# echo "${ver/replacement/replace_with}"
+
+string="/refs/tags/v/v0.1.0"
+result="${string##*/v}"
+echo "$result"
+
+read -p "Press Enter to continue" </dev/tty
